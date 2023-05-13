e7016695df6d0095483ed6cfa83a1d130766b12c
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2023-05-13 07:34:19 +0200

    Update docker-run.bash

--- docker-run.bash
+++ docker-run.bash
@@ -1,3 +1,3 @@
 docker build --no-cache --rm -f Containerfile -t spring:demo .
 docker run --interactive --tty -p 8080:8080 spring:demo
-echo "browse http://localhost:8080/hello"
+echo "browse http://localhost:8888/hello?name=Test"
