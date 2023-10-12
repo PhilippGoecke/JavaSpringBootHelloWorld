0655a0728d13a12f6ca08c4c04cd917070acca25
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2023-10-13 01:23:30 +0200

    Update and rename docker-run.bash to docker-run-gradle.bash

--- docker-run-gradle.bash
+++ docker-run-gradle.bash
@@ -1,4 +1,3 @@
 docker build --no-cache --rm -f Containerfile -t spring:demo .
-# docker build --no-cache --rm -f Containerfile.maven -t spring:demo .
 docker run --interactive --tty -p 8888:8888 spring:demo
 echo "browse http://localhost:8888/hello?name=Test"
