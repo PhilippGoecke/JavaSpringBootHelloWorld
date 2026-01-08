775d75813aec24f3d7e1dc4e3973f865ca242c81
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-01-08 21:59:21 +0100

    Refactor podman commands for consistency

--- podman-run-maven.bash
+++ podman-run-maven.bash
@@ -1,3 +1,3 @@
-podman build --no-cache --rm -f Containerfile.maven -t spring:demo .
-podman run --interactive --tty -p 8889:8888 spring:demo
+podman build --no-cache --rm --file Containerfile.maven --tag spring:demo .
+podman run --interactive --tty --publish 8889:8888 spring:demo
 echo "browse http://localhost:8889/hello?name=Test"
