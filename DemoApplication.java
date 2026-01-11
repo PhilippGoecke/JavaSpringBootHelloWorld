e02a08c37040719db7d81e35b571551b11bd70b0
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-01-11 22:09:28 +0100

    Simplify hello endpoint response
    Removed build tool detection from hello endpoint.

--- DemoApplication.java
+++ DemoApplication.java
@@ -13,21 +13,6 @@ public class DemoApplication {
     }
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      String buildTool = "Unknown";
-      // Check if Maven pom.properties exists in classpath
-      if (getClass().getResource("/META-INF/maven/com.example/demo/pom.properties") != null) {
-          buildTool = "Maven";
-      } else if (getClass().getResource("/META-INF/gradle/") != null) {
-          buildTool = "Gradle";
-      } else {
-          buildTool = "Unpackaged/IDE";
-      }
-      
-      String springVersion = SpringApplication.class.getPackage().getImplementationVersion();
-      if (springVersion == null) {
-          springVersion = "Unknown";
-      }
-      
-      return String.format("Hello %s!<br>(Spring Version: %s)<br>(Build Tool: %s)", name, springVersion, buildTool);
+      return String.format("Hello %s!<br>(Spring Version: %s)", name, SpringApplication.class.getPackage().getImplementationVersion());
     }
 }
